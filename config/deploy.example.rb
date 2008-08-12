8e88df1ddc5871471c5b7f21ee831cf29b9f58ce
Jeff Rafter <jeff@baobabhealth.org>
2008-08-12 20:22:45 +0200

    Capistrano deployments work now... but when you cap deploy:start you will need to hit ctrl+c for the moment, but the daemon is still running

--- config/deploy.example.rb
+++ config/deploy.example.rb
@@ -1,7 +1,7 @@
 set :application, "patatat"
 set :domain, CLI.ui.ask("Domain you are deploying to (IP Address or Hostname): ")
 set :deploy_to, "/var/www/#{application}"
-set :repository,  "git://github.com/mikeymckay/patatat.git"
+set :repository,  "git://github.com/jeffrafter/patatat.git"
 set :scm, :git
 set :keep_releases, 2
 set :user, "deploy"
@@ -18,26 +18,45 @@ namespace :init do
     desc "Create shared configuration"
     task :patatat do
       if Capistrano::CLI.ui.ask("Create patatat configuration? (y/n): ") == 'y'
-        set :twitter_name, Capistrano::CLI.ui.ask("twitter user: ")
-				set :twitter_pass, Capistrano::CLI.password_prompt("twitter password: ")			
-				patatat_configuration =<<-EOF
+        set :twitter_user, Capistrano::CLI.ui.ask("twitter user: ")
+        set :twitter_pass, Capistrano::CLI.password_prompt("twitter password: ")      
+        patatat_configuration =<<-EOF
 ---
 twitter:
   username: #{twitter_user}
   password: #{twitter_pass}
 EOF
-				sudo "mkdir -p #{shared_path}"
+        sudo "mkdir -p #{shared_path}"
         sudo "chown deploy:deploy #{shared_path}"
-        run <<-CMD
-				  mkdir -p #{shared_path}/config &&
-				  mkdir -p #{shared_path}/yoke &&
-				  mkdir -p #{shared_path}/database &&
-				  mkdir -p #{shared_path}/log &&
-				  mkdir -p #{shared_path}/pids
-        CMD  
-				put patatat_configuration, "#{shared_path}/config/patatat.conf"
-		  end		
+        run <<-COMMAND
+          mkdir -p #{shared_path}/config &&
+          mkdir -p #{shared_path}/yoke &&
+          mkdir -p #{shared_path}/database &&
+          mkdir -p #{shared_path}/tmp &&
+          mkdir -p #{shared_path}/tmp/pids &&
+          mkdir -p #{shared_path}/log &&
+          touch #{shared_path}/log/yell.log
+        COMMAND
+        put patatat_configuration, "#{shared_path}/config/patatat.conf"
+      end    
     end
+    
+    desc "Symlink shared configurations to current"
+    task :localize, :roles => [:app] do
+      run <<-COMMAND
+        ln -nsf #{shared_path}/config/patatat.conf #{release_path}/config/patatat.conf &&
+        ln -nsf #{shared_path}/yoke #{release_path}/yoke &&
+        ln -nsf #{shared_path}/database #{release_path}/database &&
+        ln -nsf #{shared_path}/log #{release_path}/log && 
+        ln -nsf #{shared_path}/tmp/pids #{release_path}/tmp/pids 
+      COMMAND
+
+      sudo "chown -R patatat:patatat #{release_path}/config/"
+      sudo "chown -R patatat:patatat #{release_path}/yoke/"
+      sudo "chown -R patatat:patatat #{release_path}/database/"
+      sudo "chown -R patatat:patatat #{release_path}/log/"
+      sudo "chown -R patatat:patatat #{release_path}/tmp/pids/"
+    end 		    
   end
 end
 
@@ -45,7 +64,7 @@ end
 namespace :deploy do
   desc "Start patatat"
   task :start do
-    run  "cd #{current_path} && sudo su - patatat script/patatat"
+    sudo  "su - patatat -c \"cd /var/www/patatat/current && script/patatat\" &"
   end
   
   desc "Stop patatat"
@@ -61,12 +80,12 @@ namespace :deploy do
   desc "Finalize the update"
   task :finalize_update, :except => { :no_release => true } do
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
-    run <<-CMD
+    run <<-COMMAND
       rm -rf #{latest_release}/log #{latest_release}/tmp/pids &&
       mkdir -p #{latest_release}/tmp &&
       ln -s #{shared_path}/log #{latest_release}/log &&
-      ln -s #{shared_path}/pids #{latest_release}/tmp/pids
-    CMD
+      ln -s #{shared_path}/tmp/pids #{latest_release}/tmp/pids
+    COMMAND
   end  
 
 end
@@ -74,19 +93,7 @@ end
 # == TASKS =====================================================================
 after "deploy", "deploy:cleanup"
 after "deploy:setup", "init:config:patatat"
+after "deploy:symlink", "init:config:localize"
 
 task :after_update_code do
-  run <<-CMD
-    ln -nsf #{shared_path}/config/patatat.conf #{release_path}/config/patatat.conf &&
-    ln -nsf #{shared_path}/yoke #{release_path}/yoke &&
-    ln -nsf #{shared_path}/database #{release_path}/database &&
-    ln -nsf #{shared_path}/log #{release_path}/log && 
-    ln -nsf #{shared_path}/pids #{release_path}/pids 
-  CMD  
-
-  sudo "chown patatat:patatat #{release_path}/config -R"
-  sudo "chown patatat:patatat #{release_path}/yoke -R"
-  sudo "chown patatat:patatat #{release_path}/database -R"
-  sudo "chown patatat:patatat #{release_path}/log -R"
-  sudo "chown patatat:patatat #{release_path}/tmp/pids -R"
 end
